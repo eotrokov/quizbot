15ec40b6b7f18d2cb929649e3c866b0f1f33ff0b
evgeny otrokov <otrokov@me.com>
2019-10-17 20:55:49 +0300

    services

---
+++ QuizBot.Service.Contract/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using QuizBot.Model;
+
+namespace QuizBot.Service.Contract
+{
+    public interface IUserService
+    {
+        User GetUserById(int fromId);
+        bool SaveUser(User user);
+    }
+}
---
+++ QuizBot.Service/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using QuizBot.Model;
+using QuizBot.Repository.Contract;
+using QuizBot.Service.Contract;
+
+namespace QuizBot.Service
+{
+    public class UserService : IUserService
+    {
+        private IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User GetUserById(int fromId) => _userRepository.GetUserById(fromId);
+        public bool SaveUser(User user) => _userRepository.SaveUser(user);
+    }
+}
